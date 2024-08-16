16d593528d1739034f1bbee64d3dd6ce75c05dac
mgsrood <mgsrood@gmail.com>
2024-08-16 14:39:23 +0200

    Update 2024-08-16 14:39:23

---
+++ subscription_check/main.py
@@ -0,0 +1,56 @@
+from woocommerce import API
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from modules.utils import filter_subscriptions_with_price_difference, get_active_subscriptions, send_email_with_attachment
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    # Load environment variables
+    woocommerce_url = os.getenv('WOOCOMMERCE_URL')
+    consumer_key = os.getenv('WOOCOMMERCE_CONSUMER_KEY')
+    consumer_secret = os.getenv('WOOCOMMERCE_CONSUMER_SECRET')
+
+    # Configuring the WooCommerce API
+    wcapi = API(
+        url=woocommerce_url,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        version="wc/v3",
+        timeout=60
+    )
+
+    # SMTP configuratie
+    smtp_server = os.getenv('MAIL_SMTP_SERVER')
+    smtp_port = os.getenv('MAIL_SMTP_PORT')
+    sender_email = os.getenv("MAIL_SENDER_EMAIL")
+    smtp_username = sender_email
+    smtp_password = os.getenv("MAIL_SENDER_PASSWORD")
+    recipient_email = sender_email
+
+
+    # Haal alle actieve abonnementen op
+    subscriptions = get_active_subscriptions(wcapi)
+    filtered_subscriptions = filter_subscriptions_with_price_difference(subscriptions)
+
+    # Creëer een DataFrame
+    df = pd.DataFrame(filtered_subscriptions)
+
+    # Exclude specific id's from DataFrame
+    df = df[~df['id'].isin([4889, 7074, 34540])] # Maria Rood, Annemiek Bakker, Mirjam van der Meer
+
+    # Optioneel: Exporteer naar een Excel-bestand
+    file_path = "afwijkende_abonnementen.xlsx"
+    df.to_excel(file_path, index=False)
+
+    send_email_with_attachment(sender_email, recipient_email, smtp_server, smtp_port, smtp_username, smtp_password, file_path)
+
+    # Verwijder het Excel-bestand na verzending
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"Het bestand '{file_path}' is succesvol verwijderd.")
+    else:
+        print(f"Het bestand '{file_path}' kon niet worden gevonden.")
+
+    print("E-mail succesvol verstuurd naar: " + recipient_email + ".")
---
+++ subscription_check/modules/utils.py
@@ -0,0 +1,62 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import smtplib
+
+def filter_subscriptions_with_price_difference(subscriptions, target_price=29.99):
+    data = []
+
+    for subscription in subscriptions:
+        for item in subscription.get('line_items', []):
+            if float(item['price']) != target_price:
+                data.append({
+                    'Abonnement ID': subscription['id'],
+                    'Klant Naam': f"{subscription['billing']['first_name']} {subscription['billing']['last_name']}",
+                    'Product': item['name'],
+                    'Prijs': float(item['price'])
+                })
+
+    return data
+
+def get_active_subscriptions(wcapi):
+    subscriptions = []
+    page = 1
+
+    while True:
+        response = wcapi.get(f"subscriptions?status=active&page={page}&per_page=100").json()
+        if not response:
+            break
+        subscriptions.extend(response)
+        print(f"Page {page}: {len(response)} abonnementen gevonden")
+        page += 1
+
+    return subscriptions
+
+def send_email_with_attachment(sender_email, recipient_email, smtp_server, smtp_port, smtp_username, smtp_password, file_path):
+    # Maak een MIMEMultipart object aan
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = 'Overzicht van Abonnementen met Afwijkende Prijs'
+
+    # Voeg een tekstbericht toe
+    body = "In de bijlage vind je het overzicht van abonnementen waarbij de prijs afwijkt van €29,99."
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Voeg de Excel-bijlage toe
+    with open(file_path, "rb") as attachment:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f"attachment; filename= {file_path}")
+        msg.attach(part)
+
+    # Verstuur de e-mail via SMTP
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    text = msg.as_string()
+    server.sendmail(sender_email, recipient_email, text)
+    server.quit()
