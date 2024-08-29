5833260249a8feb375933d54d1d11760c054e9cb
mgsrood <mgsrood@gmail.com>
2024-08-29 15:00:27 +0200

    Update 2024-08-29 15:00:27

---
+++ webhooks/modules/facebook_utils.py
@@ -0,0 +1,74 @@
+import requests
+from facebook_business.api import FacebookAdsApi
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.exceptions import FacebookRequestError
+from dotenv import load_dotenv
+import os
+
+def update_env_file(new_token):
+    with open('.env', 'r') as file:
+        lines = file.readlines()
+
+    with open('.env', 'w') as file:
+        for line in lines:
+            if line.startswith('FACEBOOK_LONG_TERM_ACCESS_TOKEN'):
+                file.write(f"FACEBOOK_LONG_TERM_ACCESS_TOKEN={new_token}\n")
+            else:
+                file.write(line)
+
+def renew_access_token(app_id, app_secret, long_term_token):
+    url = f"https://graph.facebook.com/v20.0/oauth/access_token?grant_type=fb_exchange_token&client_id={app_id}&client_secret={app_secret}&fb_exchange_token={long_term_token}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        new_token = response.json()['access_token']
+        update_env_file(new_token)
+        print(f"Access token renewed: {new_token}")
+        return new_token
+    else:
+        raise Exception(f"Failed to renew access token: {response.text}")
+    
+def get_facebook_custom_audiences():
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Get the GCP keys
+    app_id = os.getenv('FACEBOOK_APP_ID')
+    app_secret = os.getenv('FACEBOOK_APP_SECRET')
+    long_term_token = os.getenv('FACEBOOK_LONG_TERM_ACCESS_TOKEN')
+    ad_account_id = os.getenv('FACEBOOK_AD_ACCOUNT_ID')
+
+    try:
+        FacebookAdsApi.init(app_id=app_id, app_secret=app_secret, access_token=long_term_token)
+    except Exception as e:
+        if 'Error validating access token' in str(e):
+            print("Access token expired, renewing token...")
+            new_token = renew_access_token()
+            FacebookAdsApi.init(app_id=app_id, app_secret=app_secret, access_token=new_token)
+        else:
+            raise
+
+    # Maak een AdAccount object aan
+    account = AdAccount(ad_account_id)
+
+    # Specificeer de velden die je wilt ophalen
+    fields = ['name', 'id']
+
+    try:
+        # Haal alle Custom Audiences op voor dit advertentieaccount
+        audiences = account.get_custom_audiences(fields=fields)
+        
+        # Print de naam en ID van elke Custom Audience
+        for audience in audiences:
+            if 'name' in audience and 'id' in audience:
+                print(f"Audience Name: {audience['name']}, Audience ID: {audience['id']}")
+            else:
+                print(f"Audience data incomplete: {audience}")
+    except FacebookRequestError as e:
+        print(f"Facebook API error: {e.api_error_message()}")
+        print(f"Error type: {e.api_error_type()}")
+        print(f"Error code: {e.api_error_code()}")
+        print(f"Trace ID: {e.api_error_trace()}")
+
+
+if __name__ == '__main__':
+    get_facebook_custom_audiences()
--- webhooks/test.py
+++ webhooks/test.py
@@ -1,22 +1,26 @@
-from woocommerce import API
+from facebook_business.adobjects.customaudience import CustomAudience
+from facebook_business.api import FacebookAdsApi
 from dotenv import load_dotenv
+from modules.facebook_utils import renew_access_token
 import os
+import hashlib
+from woocommerce import API
 import json
-import requests
-from modules.woocommerce_utils import get_woocommerce_order_data
-from modules.catalog_generator import retrieve_all_products, save_catalogue_to_json
 
-
+# Load environment variables from .env file
 load_dotenv()
 
-# Load environment variables
+# Get the GCP keys
+app_id = os.getenv('FACEBOOK_APP_ID')
+app_secret = os.getenv('FACEBOOK_APP_SECRET')
+long_term_token = os.getenv('FACEBOOK_LONG_TERM_ACCESS_TOKEN')
+ad_account_id = os.getenv('FACEBOOK_AD_ACCOUNT_ID')
+custom_audience_id = os.getenv('FACEBOOK_CUSTOM_AUDIENCE_ID')
 woocommerce_url = os.getenv('WOOCOMMERCE_URL')
 consumer_key = os.getenv('WOOCOMMERCE_CONSUMER_KEY')
 consumer_secret = os.getenv('WOOCOMMERCE_CONSUMER_SECRET')
-active_campaign_api_token = os.getenv('ACTIVE_CAMPAIGN_API_TOKEN')
-active_campaign_api_url = os.getenv('ACTIVE_CAMPAIGN_API_URL')
 
-# Configuring the WooCommerce API
+# WCAPI
 wcapi = API(
     url=woocommerce_url,
     consumer_key=consumer_key,
@@ -25,13 +29,72 @@ wcapi = API(
     timeout=60
 )
 
-# Get WooCommerce Data
-order_id = '99384'
-woocommerce_data = get_woocommerce_order_data(order_id, wcapi)
-line_items = woocommerce_data['line_items']
-email = 'mgsrood@gmail.com'
+customer_id = '75'
 
-# Test productcatalogue
-product_catalogue, products = retrieve_all_products(wcapi)
-print(json.dumps(product_catalogue, indent=4))
-print(json.dumps(products, indent=4))
+# User GET Request
+customer_data = wcapi.get(f'customers/{customer_id}').json()
+
+# Account to add
+users = [
+    {
+        'email': f'{customer_data['email']}',
+        'phone_number': f'{customer_data['billing']['phone']}',
+        'first_name': f'{customer_data['billing']['first_name']}',
+        'last_name': f'{customer_data['billing']['last_name']}',
+        'city': f'{customer_data['billing']['city']}',
+        'state': f'{customer_data['billing']['state']}',
+        'country': f'{customer_data['billing']['country']}',
+        'zip': f'{customer_data['billing']['postcode']}'
+    }    ]
+
+try:
+    FacebookAdsApi.init(app_id=app_id, app_secret=app_secret, access_token=long_term_token)
+except Exception as e:
+    if 'Error validating access token' in str(e):
+        print("Access token expired, renewing token...")
+        new_token = renew_access_token()
+        FacebookAdsApi.init(app_id=app_id, app_secret=app_secret, access_token=long_term_token)
+    else:
+        raise
+
+# Definieer de Custom Audience
+custom_audience = CustomAudience(custom_audience_id)
+
+# Functie om gegevens te normaliseren en te hashen, als de waarde aanwezig is
+def normalize_and_hash(value):
+    if value:
+        # Normaliseer de waarde
+        value = value.strip().lower()
+    else:
+        value = ''  # Gebruik een lege string als waarde None is
+    
+    # Hash de waarde met SHA-256
+    return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+# Normaliseer en hash de gegevens
+hashed_users = []
+for user in users:
+    hashed_user = []
+    hashed_user.append(normalize_and_hash(user.get('email')))
+    hashed_user.append(normalize_and_hash(user.get('phone_number')))
+    hashed_user.append(normalize_and_hash(user.get('first_name')))
+    hashed_user.append(normalize_and_hash(user.get('last_name')))
+    hashed_user.append(normalize_and_hash(user.get('city')))
+    hashed_user.append(normalize_and_hash(user.get('state')))
+    hashed_user.append(normalize_and_hash(user.get('zip')))
+    hashed_user.append(normalize_and_hash(user.get('country')))
+    hashed_users.append(hashed_user)
+
+print(hashed_users)
+
+# Bepaal het schema dat overeenkomt met de volgorde van de gegevens in hashed_users
+schema = [
+    "EMAIL", "PHONE", "FN", "LN", "CT", "ST", "ZIP", "COUNTRY"
+]
+
+# Voeg gebruikers toe aan de Custom Audience
+response = custom_audience.add_users(
+    schema=schema,
+    users=hashed_users,
+    is_raw=True  # Geeft aan dat de gegevens al gehasht zijn
+)
