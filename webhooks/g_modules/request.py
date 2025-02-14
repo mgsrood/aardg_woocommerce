1247f11da9c3053c974801b70e8e56f938bae326
mgsrood <mgsrood@gmail.com>
2025-02-14 13:09:50 +0100

    Debug9

--- webhooks/g_modules/request.py
+++ webhooks/g_modules/request.py
@@ -12,7 +12,7 @@ def validate_signature(request, secret):
     computed_signature = base64.b64encode(
         hmac.new(secret.encode("utf-8"), raw_payload, hashlib.sha256).digest()
     ).decode("utf-8")
-
+    logging.info(f"Secret key: {secret}")
     logging.info(f"Raw Payload: {raw_payload}")
     logging.info(f"Computed Signature: {computed_signature}")
     logging.info(f"Received Signature: {received_signature}")
