ef665e0f4bae40e107a593c73b3907168c09f921
mgsrood <mgsrood@gmail.com>
2025-03-10 17:39:58 +0100

    Small config change

--- klantenservice_applicatie/config.py
+++ klantenservice_applicatie/config.py
@@ -23,8 +23,8 @@ class Config:
     
     # Monta API configuratie
     MONTA_API_URL = os.getenv('MONTA_API_URL', 'https://api-v6.monta.nl')
-    MONTA_API_KEY = os.getenv('MONTA_API_KEY')
-    MONTA_API_SECRET = os.getenv('MONTA_API_SECRET')
+    MONTA_USERNAME = os.getenv('MONTA_USERNAME')
+    MONTA_PASSWORD = os.getenv('MONTA_PASSWORD')
     
     # Gebruik SQLite als standaard
     USE_SQLITE = os.getenv('USE_SQLITE', 'true').lower() == 'true' 
